d075eb41bc37697bd3f37695080c4de1c799b415
panzo <administrator@192.168.1.22>
2024-02-06 01:02:37 +0100

    Prerelease version

--- HtmlGenerator.js
+++ HtmlGenerator.js
@@ -1,3 +1,7 @@
+import { cross } from "./assets/cross";
+import { leafs } from "./assets/lisce";
+import { mourning } from "./assets/mourning";
+
 export const generateHtml = (image, name, surname, settedValues) => {
   const male = settedValues[0] === "Musko" ? true : false;
 
@@ -43,13 +47,16 @@ export const generateHtml = (image, name, surname, settedValues) => {
           height: 180px !important;
           object-fit: cover !important;
           position: absolute !important;
-            top: 135px;
+            top: 160px;
             left: 10px;
           z-index: 1 !important;
         }
-  
+        .moruning-svg {
+            position: absolute !important;
+            bottom: 220px;
+            left: 247px;
+        }
         .content {
-          /* Dodajte stilove za ostale elemente kao što su <p> i <div> */
           height: 100%;
           width: 100%;
           display: flex;
@@ -61,54 +68,73 @@ export const generateHtml = (image, name, surname, settedValues) => {
         p {
             margin-top:-15px;
             padding-top:0px;
-            font-size: 24px;
+            font-size: 24px !important;
         }
 
         .bolded {
             font-weight: bold;
-            font-size: 24px;
+            font-size: 20px !important;
             padding-top: 0px;
             padding-bottom: 0px;
             margin-top: -10px;
             // margin-bottom: px;
             line-height: 1.02;
+            width: 80%;
+            // margin-left: -45px;
+            position: absolute;
+            top:calc(50% + 46px);
+            left: 60px;
+
+
 
         }
         .fullname {
-            width: 97%;
+            width: 80%;
             border-bottom: 4px solid black;
-            font-size: 50px;
+            font-size: 50px !important;
             font-weight: bold;
             margin-bottom: 17px;
             text-transform: uppercase;
             text-align: center;
+            // margin-left: -45px;
+            position: absolute;
+            top:calc(50% - 17px);
+            left:60px;
 
         }
         .mourning {
-            margin-top: -15px;
-            padding-bottom: 4px;
+            position: absolute;
+            font-size: 16px !important;
+            top: calc(50% + 155px);
+            left:320px;
         }
         .mourning-content {
-            width: 100%;
+            width: 80%;
             text-align: center;
             position: absolute; /* Dodato */
-            top: 547px; /* Dodato - postavlja se na dno roditeljskog kontejnera */
             overflow: hidden; /* Ovo će sprečiti da dugi tekst izlazi iz okvira .mourning-content */
             white-space: pre-line;
+            // margin-left: -45px;
+            font-size: 18px !important;
+            top: calc(50% + 178px);
+            left: 60px;
+
         }
           
         .light-text {
             font-weight: 200 !important;
-            font-size: 20px;
-            margin-bottom: 13px;
+            font-size: 17px !important;
             width: 60%;
+            position: absolute;
+            top: calc(50% - 53px);
 
         }
         .birth-death-years {
             font-weight: bold;
-            font-size: 32px;
-            margin-bottom:13px;
-            margin-top:80px;
+            font-size: 32px !important;
+            position: absolute;
+            top:calc(50% - 83px);
+            left:291px;
         }
       </style>
     </head>
@@ -117,6 +143,11 @@ export const generateHtml = (image, name, surname, settedValues) => {
   
       <div class="content">
       <img src="data:image/jpeg;base64,${image.base64}" />
+      <div>
+${cross}
+${mourning}
+${leafs}
+      </div>
       <p class="birth-death-years">${onlyYearsOfBirth}. - ${onlyYearsOfDeath}.</p>
         <p class="light-text">Родбини, комшијама и пријатељима јављамо тужну вијест да је ${
           male ? "наш драги и никад прежаљени" : "наша драга и никад прежаљена"
@@ -124,7 +155,7 @@ export const generateHtml = (image, name, surname, settedValues) => {
         <p class="fullname">${name} ${surname}</p>
         <p class="bolded">${male ? "Преминуо" : "Преминула"} дана ${
     settedValues[3]
-  } у ${age}. години живота, након ${
+  }. у ${age}. години живота, након ${
     settedValues[1] === "Nakon krace bolesti"
       ? "краће болести."
       : settedValues[1] === "Nakon duze bolesti"
